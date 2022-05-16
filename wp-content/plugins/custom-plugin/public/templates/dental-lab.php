b78c6d53478f6f918fbe116df33d8db1817de834
DeepakSecureWeb <deepaksecureweb@gmail.com>
2022-05-16 18:09:29 +0530

    designe changes

--- wp-content/plugins/custom-plugin/public/templates/dental-lab.php
+++ wp-content/plugins/custom-plugin/public/templates/dental-lab.php
@@ -1,108 +1,200 @@
 <div id="custom-poup-form" class="custom-poup-form" style="display: none;">
-	
+	<div class="popup_wrap">
+		<span class="close_btn" onClick="closepoup()">x</span>
 		<form method="post">
-			<h4>Order Details</h4>
-			
-			<div class="form-group">
-				<input class="form-control" type="text" name="cs_dentist_name" readonly placeholder="Dentist Name">
-			</div>
-			
-			<div class="form-group">
-				<input class="form-control" type="text" name="cs_clinic_name" readonly placeholder="Clinic Name">
-			</div>
+			<div class="form_step">
+				<h4>Order Details</h4>	
+				<div class="d-flex">		
+					<div class="form-group w-50">
+						<input class="form-control" type="text" name="cs_dentist_name" readonly placeholder="Dentist Name">
+					</div>
+					
+					<div class="form-group w-50">
+						<input class="form-control" type="text" name="cs_clinic_name" readonly placeholder="Clinic Name">
+					</div>
 
-			<div class="form-group">
-				<input class="form-control" type="text" name="cs_email" placeholder="Email">
-			</div>
+					<div class="form-group w-50">
+						<input class="form-control" type="text" name="cs_email" placeholder="Email">
+					</div>
 
-			<div class="form-group">
-				<input class="form-control" type="text" name="cs_phone" placeholder="Phone">
-			</div>
+					<div class="form-group w-50">
+						<input class="form-control" type="text" name="cs_phone" placeholder="Phone">
+					</div>
 
-			<div class="form-group">
-				<input class="form-control" type="date" name="cs_date" placeholder="Date">
-			</div>
+					<div class="form-group w-100">
+						<input class="form-control" type="date" name="cs_date" placeholder="Date">
+					</div>
 
-			<div class="form-group">
-				<input class="form-control" type="text" name="cs_patient_name" placeholder="Patient Name">
-			</div>
+					<div class="form-group w-50">
+						<input class="form-control" type="text" name="cs_patient_name" placeholder="Patient Name">
+					</div>
 
-			<div class="form-group">
-				<input class="form-control" type="text" name="cs_patient_age" placeholder="Patient Age">
-			</div>
+					<div class="form-group w-50">
+						<input class="form-control" type="text" name="cs_patient_age" placeholder="Patient Age">
+					</div>
 
-			<p>Gender</p>
-			<div class="form-group">
-				<label>Male</label>
-				<input class="form-control" type="radio" name="cs_gender" value="Male">
-				<label>Female</label>
-				<input class="form-control" type="radio" name="cs_gender" value="Female">
-			</div>
+					<div class="form-group">
+						<p class="w-100">Gender</p>
+						<div class="d-flex">
+							<div class="gender">
+								<label class="gender_filed">Male
+								  <input class="form-control" type="radio" name="cs_gender" value="Male">
+								  <span class="checkmark"></span>
+								</label>
+							</div>
+							<div class="gender">
+								<label class="gender_filed">Female
+								  <input class="form-control" type="radio" name="cs_gender" value="Female">
+								  <span class="checkmark"></span>
+								</label>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div><!--form_step-->
 
-			<h4>Restoration Type</h4>
-			<div class="form-group">
-				<label>Crown</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Crown">
+			<div class="form_step">
+				<h4>Restoration Type</h4>
+				<div class="form-group">
+					<div class="d-flex">
+						<div class="w-50">
+							<label class="gender_filed">Crown
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Crown">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Post and core</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Post and core">
+						<div class="w-50">
+							<label class="gender_filed">Post and core
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Post and core">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Largest</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Largest">
+						<div class="w-50">
+							<label class="gender_filed">Largest
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Largest">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Bridge</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Bridge">
+						<div class="w-50">
+							<label class="gender_filed">Bridge
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Bridge">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Dignstatic wax up</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Dignstatic wax up">
+						<div class="w-50">
+							<label class="gender_filed">Dignstatic wax up
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Dignstatic wax up">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Webnar</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Webnar">
+						<div class="w-50">
+							<label class="gender_filed">Webnar
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Webnar">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Way</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Way">
+						<div class="w-50">
+							<label class="gender_filed">Way
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Way">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Guide</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Guide">
+						<div class="w-50">
+							<label class="gender_filed">Guide
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Guide">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Other</label>
-				<input class="form-control" type="radio" name="cs_restoration_type" value="Other">
-			</div>
+						<div class="w-50">
+							<label class="gender_filed">Other
+								<input class="form-control" type="radio" name="cs_restoration_type" value="Other">
+								<span class="checkmark"></span>
+							</label>
+						</div>
+					</div>
+				</div>
 
-			<div class="form-group">
-				<label>Please add implant system</label>
-				<input class="form-control" type="text" name="cs_implant_system" placeholder="Please add implant system">
-			</div>
+				<div class="form-group">
+					<label>Please add implant system</label>
+					<input class="form-control" type="text" name="cs_implant_system" placeholder="Please add implant system">
+				</div>
+			</div><!--form_step-->
 
+			<div class="form_step">
+				<h4>Material Type</h4>
+				<div class="form-group">
+					<div class="d-flex">
+						<div class="w-50">
+							<label class="gender_filed">Crown
+								<input class="form-control" type="radio" name="cs_material_type" value="Crown">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-			<h4>Material Type</h4>
-			<div class="form-group">
-				<label>Crown</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Crown">
+						<div class="w-50">
+							<label class="gender_filed">Post and core
+								<input class="form-control" type="radio" name="cs_material_type" value="Post and core">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Post and core</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Post and core">
+						<div class="w-50">
+							<label class="gender_filed">Largest
+								<input class="form-control" type="radio" name="cs_material_type" value="Largest">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Largest</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Largest">
+						<div class="w-50">
+							<label class="gender_filed">Bridge
+								<input class="form-control" type="radio" name="cs_material_type" value="Bridge">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Bridge</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Bridge">
+						<div class="w-50">
+							<label class="gender_filed">Dignstatic wax up
+								<input class="form-control" type="radio" name="cs_material_type" value="Dignstatic wax up">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Dignstatic wax up</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Dignstatic wax up">
+						<div class="w-50">
+							<label class="gender_filed">Webnar
+								<input class="form-control" type="radio" name="cs_material_type" value="Webnar">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Webnar</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Webnar">
+						<div class="w-50">
+							<label class="gender_filed">Way
+								<input class="form-control" type="radio" name="cs_material_type" value="Way">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Way</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Way">
+						<div class="w-50">
+							<label class="gender_filed">Guide
+								<input class="form-control" type="radio" name="cs_material_type" value="Guide">
+								<span class="checkmark"></span>
+							</label>
+						</div>
 
-				<label>Guide</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Guide">
-
-				<label>Other</label>
-				<input class="form-control" type="radio" name="cs_material_type" value="Other">
+						<div class="w-50">
+							<label class="gender_filed">Other
+								<input class="form-control" type="radio" name="cs_material_type" value="Other">
+								<span class="checkmark"></span>
+							</label>
+						</div>
+				</div>
 			</div>
 
 			<div class="form-group">
@@ -111,5 +203,5 @@
 
 		</form>	
 
-		<span onClick="closepoup()">x</span>
+	</div>
 </div>
